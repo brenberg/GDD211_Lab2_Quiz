42fc74fa9ebbe5ba8f61c0516b1cb5f9022199a9
brenberg <75150191+brenberg@users.noreply.github.com>
2021-02-22 16:01:30 -0500

    done

---
+++ Assets/Scripts/GameScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameScript : MonoBehaviour
+{
+    public string playerName2;
+    public int playerLegal;
+
+    public Text tellPlayer;
+    public Slider mooseColor;
+    public Slider mooseSize;
+    public Slider mooseRotate;
+    public GameObject Moose;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Color32[] Colors;
+
+        Colors = new Color32[4];
+        Colors[0] = new Color32(185, 0, 185, 255);
+        Colors[1] = new Color32(0, 127, 255, 255);
+        Colors[2] = new Color32(0, 0, 255, 255);
+        Colors[3] = new Color32(0, 255, 255, 255);
+
+        playerName2 = PlayerPrefs.GetString("ThisPlayer", "playerName");
+        playerLegal = PlayerPrefs.GetInt("ThisAge");
+
+        tellPlayer.text = "Hi " + playerName2 + " your age is " + playerLegal;
+
+        float newMooseSize = mooseSize.value;
+        Moose.transform.localScale = new Vector2(newMooseSize, newMooseSize);
+
+        float mooseRotValue = (mooseRotate.value * 90f) - 45;
+        Moose.transform.eulerAngles = new Vector3(0, 0, mooseRotValue);
+
+        float newMooseColor = mooseColor.value;
+        Moose.GetComponent<Image>().color = Colors[(int)newMooseColor];
+    }
+}
---
+++ Assets/Scripts/Scene1.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Scene1 : MonoBehaviour
+{
+    public InputField playerHandle;
+    public InputField playerAge;
+    public string playerName;
+    public int myAge;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerName = playerHandle.text;
+        myAge = int.Parse(playerAge.text);
+
+        Debug.Log("name and age of player is " + playerName + myAge);
+
+        PlayerPrefs.SetString("ThisPlayer", playerName);
+        PlayerPrefs.SetInt("ThisAge", myAge);
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
